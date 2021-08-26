3d821d6880f4f91d8c00b79c6fa1499acb15784e
Flavie Clouet <flavie.clouet@gmail.com>
2021-08-26 16:02:11 +0200

    amelioration page accueil, marchand, creationHero et chargerHero

---
+++ projet-rpg-html/creerHero.js
@@ -0,0 +1,15 @@
+function validerCreationHero() {
+ let ptAtk = document.querySelector('input [type="checkbox atk"]').value;
+ console.log(ptAtk);
+}
+
+// function sauvegarderHero(Hero){
+//     let heroes = JSON.parse(localStorage.getItem('heroes'));
+
+//     if (heroes == null) {
+//         heroes = [];
+//     }
+//     heroes.push(Hero);
+
+//     localStorage.setItem('heroes', JSON.stringify(heroes));
+// }
---
+++ projet-rpg-html/son.js
@@ -0,0 +1,51 @@
+//Clique sur le bouton "sound_offr"
+document.querySelector('#sound_off')
+    .addEventListener('click', () => {
+        //Changte le volume a 0
+        document.getElementById('accueil').volume=0;
+        document.getElementById('sound_off').style.display='none';
+        document.getElementById('sound_on').style.display='inline';
+});
+
+document.querySelector('#sound_on').addEventListener('click', () => {
+    document.getElementById('accueil').volume=0.1;
+    document.getElementById('sound_off').style.display='inline';
+    document.getElementById('sound_on').style.display='none';
+});
+
+document.querySelector('#pause').addEventListener('click', () => {
+    document.getElementById('pause').style.display='none';
+    document.getElementById('play').style.display='inline';
+    document.getElementById('accueil').pause();
+});
+
+document.querySelector('#play').addEventListener('click', () => {
+    document.getElementById('play').style.display='none';
+    document.getElementById('pause').style.display='inline';
+    document.getElementById('accueil').play();
+});
+
+//Musique page camp
+document.querySelector('a[href="#tente"]').addEventListener('click', () => {
+    document.getElementById('sound_camp_fire').play();
+    document.getElementById('sound_camp_fire').volume=0.2;
+    document.getElementById('accueil').pause();
+})
+
+document.querySelector('#close_tente').addEventListener('click', () => {
+    document.getElementById('sound_camp_fire').pause();
+    document.getElementById('sound_camp_fire').currentTime=0;
+    document.getElementById('accueil').play();
+})
+
+//Musique page taverne
+document.querySelector('a[href="#taverne"]').addEventListener('click', () => {
+    document.getElementById('sound_taverne').play();
+    document.getElementById('sound_taverne').volume=0.2;
+    document.getElementById('accueil').pause();
+})
+document.querySelector('#close_taverne').addEventListener('click', () => {
+    document.getElementById('sound_taverne').pause();
+    document.getElementById('sound_taverne').currentTime=0;
+    document.getElementById('accueil').play();
+})
