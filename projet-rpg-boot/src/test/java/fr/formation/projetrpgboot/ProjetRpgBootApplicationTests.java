afaa23c12a9d71096001a13c38403d568e02de55
Flavie Clouet <flavie.clouet@gmail.com>
2021-09-08 14:13:27 +0200

    ajout projet-rpg-boot

---
+++ projet-rpg-boot/src/test/java/fr/formation/projetrpgboot/ProjetRpgBootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.projetrpgboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetRpgBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
